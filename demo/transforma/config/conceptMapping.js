05273e1940d9a429eca0c85aa1fb5147f8f8d77f
angie <konzeptmeister@gmail.com>
2025-05-09 19:10:40 +0200

    Attempt to redeploy transforma

--- demo/transforma/config/conceptMapping.js
+++
@@ -1,2 +0,0 @@
-const toolsPage_conceptMapping =  {
-};
--- demo/transforma/config/menu-items.js
+++
@@ -1,8 +0,0 @@
-const toolsPage_menuItems = {
-  "_id": "56002c460faa9de708f37c33",
-  "node_id": null,
-  "menu_label": "Home",
-  "caption": null,
-  "url": null,
-  "children": []
-};
